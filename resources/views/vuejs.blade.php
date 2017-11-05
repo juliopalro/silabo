be7534e7155b21bb6dcb6964f417a68462148d31
Julio César Palacios Rojas <juliopalro@gmail.com>
2017-11-05 12:49:36 -0500

    Creando la vista para usar vuejs

---
+++ resources/views/vuejs.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Curso de Diseño web I - II</title>
+    <meta name="author" content="@juliopalro">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="{{ asset('assets/css/bundle.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
+</head>
+<body>
+    <div id="app"></div>
+
+    <script src="{{ asset('assets/js/bundle.js' }}"></script>
+    <script src="{{ asset('assets/js/app.js' }}"></script>
+</body>
+</html>
