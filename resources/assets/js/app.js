60930a44655b423633fa2dca39fc72fca81588df
Julio César Palacios Rojas <juliopalro@gmail.com>
2017-11-18 06:53:46 -0500

    Archivo de configuración para VUE

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,4 +1,24 @@
-// script para la web
-const app = new Vue({
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Routers from './routers'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes:Routers
+})
+
+new Vue({
+  el: '#app',
   router
-}).$mount('#app')
+})
