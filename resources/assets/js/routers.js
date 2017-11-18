a9c255e352f232c177eef57800073a0ac5de99d6
Julio César Palacios Rojas <juliopalro@gmail.com>
2017-11-18 06:56:44 -0500

    Archivo de configuración para las rutas del sistema

---
+++ resources/assets/js/routers.js
@@ -0,0 +1,7 @@
+import Home from './components/Home'
+import Teachers from './components/Teachers'
+
+export default [
+  {path: '/', name: 'home', component: Home},
+  {path: '/teachers', name: 'teachers', component: Teachers}
+]
